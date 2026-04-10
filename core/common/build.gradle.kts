22eaa9a9e78ba5b89e76c567064914be7684ea35
jingkarqi <SoraET2005@outlook.com>
2026-04-10 18:03:38 +0800

    chore: bootstrap heavy multi-module skeleton

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.sora.omniclaw.core.common"
+
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 28
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
